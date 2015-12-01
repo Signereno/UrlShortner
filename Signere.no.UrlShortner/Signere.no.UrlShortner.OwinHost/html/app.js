622d60bd532f63727b94fc7e259b435f92b012e3
Rune Synnevåg <runes@unipluss.no>
2015-12-01 08:12:28 +0100

    Oppdatert web gui med prefix

--- Signere.no.UrlShortner/Signere.no.UrlShortner.OwinHost/html/app.js
+++ Signere.no.UrlShortner/Signere.no.UrlShortner.OwinHost/html/app.js
@@ -2,12 +2,13 @@ var app=angular.module("urlshortner",[])
 
 .controller('main',['$http',function($http){
   this.Title='Test';
-  this.Url='';
+  this.Url = '';
+ this.Prefix = undefined;
   this.ShortUrl=undefined;
   this.AccessToken=undefined;
   this.Create=function(){
     var self=this;
-      $http.post('https://s.signere.no', { Url: this.Url }).then(function(result) {
+      $http.post('https://s.signere.no', { Url: this.Url,Prefix:this.Prefix }).then(function(result) {
           console.log(result.data);
           self.ShortUrl = result.data.ShortUrl;
           self.AccessToken = result.data.AccessToken;
